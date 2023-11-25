eb8cb824bae9770d29d2cf5bbd093a774460de89
dimnov <dimnov1512@gmail.com>
2023-11-25 20:05:23 +0200

    Add backround change buttons

--- 02bgChanger/src/App.jsx
+++ 02bgChanger/src/App.jsx
@@ -1,10 +1,40 @@
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("olive");
+
+  const changeColor = (newColor) => {
+    setColor(newColor);
+  };
+
   return (
-    <>
-      <h1 className="bg-green-500 text-3xl">Background changer app</h1>
-    </>
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => changeColor("red")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
+          >
+            Red
+          </button>
+          <button
+            onClick={() => changeColor("green")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
+          >
+            Green
+          </button>
+          <button
+            onClick={() => changeColor("blue")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
+          >
+            Blue
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
